514034c861adbba21a1a052b51e89150ad342a37
Chanhwi Lee <chanhwi.lee6@gmail.com>
2024-08-31 22:16:42 +0900

    🎉 [BEGIN] : Flower_Dance 시작

---
+++ Assets/02_Scripts/Flower.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public int score;
+
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() { }
+}
---
+++ Assets/02_Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public int speed;
+    Rigidbody2D rigid;
+    public Vector2 inputVec;
+
+    void Awake()
+    {
+        this.rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue input)
+    {
+        // 좌우 움직임만 받기위해 inputVec을 입력의 X 축만 사용
+        Vector2 rawInput = input.Get<Vector2>();
+        this.inputVec = new Vector2(rawInput.x, 0);
+        // 상하 좌우 입력을 모두 받으려면 아래 주석만 사용
+        // this.inputVec = input.Get<Vector2>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // TODO : 여기에 꽃이랑 충돌시 액션 작성
+    }
+}
